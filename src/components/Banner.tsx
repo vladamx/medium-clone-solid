0b86bd9cf40747bd3f0be583ea4710390bc07567
Vladimir Milojevic <vladamx.dev@gmail.com>
2023-06-05 01:18:47 +0200

    Extract home page components

---
+++ src/components/Banner.tsx
@@ -0,0 +1,10 @@
+export const Banner = () => {
+  return (
+    <div class='banner'>
+      <div class='container'>
+        <h1 class='logo-font'>conduit</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,8 @@
 import { useRouteData } from '@solidjs/router'
+import { ArticlePreview } from '../components/ArticlePreview'
+import { Banner } from '../components/Banner'
+import { FeedToggle } from '../components/FeedToggle'
+import { Sidebar } from '../components/Sidebar'
 
 import { homeRouteData } from './Home.data'
 
@@ -7,111 +11,22 @@ export const Home = () => {
 
   return (
     <div class='home-page'>
-      <div class='banner'>
-        <div class='container'>
-          <h1 class='logo-font'>conduit</h1>
-          <p>A place to share your knowledge.</p>
-        </div>
-      </div>
-      <pre>{JSON.stringify(feed()?.articles[0], null, 2)}</pre>
+      <Banner />
+
+      <pre>State: {JSON.stringify(feed.state, null, 2)}</pre>
+      <pre>Loading: {JSON.stringify(feed.loading, null, 2)}</pre>
+      <pre>Error: {JSON.stringify(feed.error ?? 'No error', null, 2)}</pre>
+      <pre>Data: {JSON.stringify(feed()?.articles[0], null, 2)}</pre>
 
       <div class='container page'>
         <div class='row'>
           <div class='col-md-9'>
-            <div class='feed-toggle'>
-              <ul class='nav nav-pills outline-active'>
-                <li class='nav-item'>
-                  <a class='nav-link disabled' href=''>
-                    Your Feed
-                  </a>
-                </li>
-                <li class='nav-item'>
-                  <a class='nav-link active' href=''>
-                    Global Feed
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div class='article-preview'>
-              <div class='article-meta'>
-                <a href='profile.html'>
-                  <img src='http://i.imgur.com/Qr71crq.jpg' />
-                </a>
-                <div class='info'>
-                  <a href='' class='author'>
-                    Eric Simons
-                  </a>
-                  <span class='date'>January 20th</span>
-                </div>
-                <button class='btn btn-outline-primary btn-sm pull-xs-right'>
-                  <i class='ion-heart'></i> 29
-                </button>
-              </div>
-              <a href='' class='preview-link'>
-                <h1>How to build webapps that scale</h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-              </a>
-            </div>
-
-            <div class='article-preview'>
-              <div class='article-meta'>
-                <a href='profile.html'>
-                  <img src='http://i.imgur.com/N4VcUeJ.jpg' />
-                </a>
-                <div class='info'>
-                  <a href='' class='author'>
-                    Albert Pai
-                  </a>
-                  <span class='date'>January 20th</span>
-                </div>
-                <button class='btn btn-outline-primary btn-sm pull-xs-right'>
-                  <i class='ion-heart'></i> 32
-                </button>
-              </div>
-              <a href='' class='preview-link'>
-                <h1>
-                  The song you won't ever stop singing. No matter how hard you
-                  try.
-                </h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-              </a>
-            </div>
+            <FeedToggle />
+            <ArticlePreview />
+            <ArticlePreview />
           </div>
-
           <div class='col-md-3'>
-            <div class='sidebar'>
-              <p>Popular Tags</p>
-
-              <div class='tag-list'>
-                <a href='' class='tag-pill tag-default'>
-                  programming
-                </a>
-                <a href='' class='tag-pill tag-default'>
-                  javascript
-                </a>
-                <a href='' class='tag-pill tag-default'>
-                  emberjs
-                </a>
-                <a href='' class='tag-pill tag-default'>
-                  angularjs
-                </a>
-                <a href='' class='tag-pill tag-default'>
-                  react
-                </a>
-                <a href='' class='tag-pill tag-default'>
-                  mean
-                </a>
-                <a href='' class='tag-pill tag-default'>
-                  node
-                </a>
-                <a href='' class='tag-pill tag-default'>
-                  rails
-                </a>
-              </div>
-            </div>
+            <Sidebar />
           </div>
         </div>
       </div>
