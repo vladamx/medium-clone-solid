207296563c9da4d0f37fa05f0efb184fb2d0f570
Vladimir Milojevic <vladamx.dev@gmail.com>
2023-06-08 00:06:20 +0200

    Integrate global feed with route data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import { useRouteData } from '@solidjs/router'
+import { parseISO } from 'date-fns'
+import { For } from 'solid-js'
 import { ArticlePreview } from '../components/ArticlePreview'
 import { Banner } from '../components/Banner'
 import { FeedToggle } from '../components/FeedToggle'
@@ -13,12 +15,6 @@ export const Home = () => {
   return (
     <div class='home-page'>
       <Banner title='conduit' description='A place to share your knowledge.' />
-
-      <pre>State: {JSON.stringify(feed.state, null, 2)}</pre>
-      <pre>Loading: {JSON.stringify(feed.loading, null, 2)}</pre>
-      <pre>Error: {JSON.stringify(feed.error ?? 'No error', null, 2)}</pre>
-      <pre>Data: {JSON.stringify(feed()?.articles[0], null, 2)}</pre>
-
       <Page>
         <div class='row'>
           <div class='col-md-9'>
@@ -26,34 +22,27 @@ export const Home = () => {
               feeds={[{ id: 'global', title: 'Global Feed' }]}
               defaultFeed='global'
             />
-            <ArticlePreview
-              author={{
-                username: 'jake',
-                image:
-                  'https://static.productionready.io/images/smiley-cyrus.jpg',
-                following: false,
-                name: 'Jacob',
-              }}
-              date='January 20th'
-              title='How to build webapps that scale'
-              slug='how-to-build-webapps-that-scale'
-              description='This is the description for the post.'
-              favoritesCount={0}
-            />
-            <ArticlePreview
-              author={{
-                username: 'jake',
-                image:
-                  'https://static.productionready.io/images/smiley-cyrus.jpg',
-                following: false,
-                name: 'Jacob',
-              }}
-              date='January 20th'
-              title='How to build webapps that scale'
-              slug='how-to-build-webapps-that-scale'
-              description='This is the description for the post.'
-              favoritesCount={0}
-            />
+            {feed.loading && <div>Loading...</div>}
+            {feed.error && <div>Error: {feed.error}</div>}
+            <For each={feed()?.articles}>
+              {article => (
+                <ArticlePreview
+                  author={{
+                    username: article.author.username,
+                    image:
+                      article.author.image ||
+                      'https://static.productionready.io/images/smiley-cyrus.jpg',
+                    following: article.author.following,
+                    name: article.author.username,
+                  }}
+                  date={parseISO(article.createdAt).toLocaleDateString()}
+                  title={article.title}
+                  slug={article.slug}
+                  description={article.description}
+                  favoritesCount={article.favoritesCount}
+                />
+              )}
+            </For>
           </div>
           <div class='col-md-3'>
             <Sidebar />
